88e2ebfec27ecbe118c42dc854e1c8643cb3770e
MichaelHogers <michael_hogers@live.nl>
2024-04-13 11:18:49 +0200

    `add_user`

--- helperDbManagement/R/qry_user.R
+++ helperDbManagement/R/qry_user.R
@@ -1,10 +1,55 @@
 #' User query
 #'
 #' This retrieves user data from the database
+#' (not always advisable to read the entire table)
 #' @export
 get_users <- function() {
     DBI::dbReadTable(
-        get_db_connection(),
-        "users"
+        conn = get_db_connection(),
+        name = "users"
     )
+}
+
+#' Add a user to the database
+#' @param user_name The user's name
+#' @param last_login The user's last login
+#' @param access_rights The user's access rights
+#' @param testing Whether to run in testing mode, will rollback
+#' the db transaction if TRUE
+#' @return true if successful
+#' @export
+add_user <- function(
+    user_name,
+    last_login,
+    access_rights,
+    testing = FALSE
+) {
+
+    entry <- data.frame(
+        user_name = user_name,
+        last_login = last_login,
+        access_rights = access_rights
+    )
+
+    conn <- get_db_connection()
+
+    DBI::dbWithTransaction(
+      conn, {
+        DBI::dbWriteTable(
+            conn = conn,
+            name = "users",
+            value = entry,
+            append = TRUE
+        )
+
+        if (testing) {
+            # rolls back the transaction
+            DBI::dbBreak()
+        }
+    })
+
+    DBI::dbDisconnect(conn)
+
+    TRUE
+
 }
--- helperDbManagement/tests/testthat/test-qry_user.R
+++ helperDbManagement/tests/testthat/test-qry_user.R
@@ -2,3 +2,13 @@ test_that("get_users() works", {
     data <- get_users()
     expect_true(is.data.frame(data))
 })
+
+test_that("", {
+    result <- add_user(
+        user_name = "test_user",
+        last_login = Sys.time(),
+        access_rights = "example",
+        testing = TRUE
+    )
+    expect_true(result)
+})
